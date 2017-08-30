00093203b06b71413ff63a26a45ad59b5d344b0e
martin sarsale <martin@properati.com>
2017-08-30 13:30:55 -0300

    Adds CustomerSource entity

---
+++ lib/prosperworks/customer_source.rb
@@ -0,0 +1,12 @@
+module ProsperWorks
+  class CustomerSource  < Base
+    extend ApiOperations::List
+
+    attr_accessor :name
+
+    def self.api_name
+      "customer_sources"
+    end
+
+  end
+end
