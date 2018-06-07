e9284d661656e8ab2b730d8cd2e4e5c6d5d2d141
Bruno Salerno <br.salerno@gmail.com>
2018-06-06 22:10:56 -0300

    Loss reasons (#25)
    * Loss reasons
    
    * LossReason inherits from Base

--- lib/prosperworks.rb
+++ lib/prosperworks.rb
@@ -33,6 +33,7 @@ require 'prosperworks/project'
 require 'prosperworks/task'
 require 'prosperworks/version'
 require 'prosperworks/webhook'
+require 'prosperworks/loss_reason'
 
 module ProsperWorks
 
